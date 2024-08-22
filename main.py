f1db68e218b1ba1161a1846a13ad1e467de57594
unknown <>
2024-08-22 21:38:06 +0700

    Menambahkan Buting Ahmad

--- main.py
+++ main.py
@@ -3,16 +3,22 @@ import streamlit as st
 # session state agar ketika pindah page tidak berubah data yang tersedia
 
 st.session_state.pindah = True
+#Mencegah datanya berubah menjadi default
 
 Homepage = st.Page("Halaman Utama/halaman_utama.py",
     title=" Profil Kelompok",
     default=True)
+#homepage itu adalah variabel
+#st.page adalah komponen di streamlit untuk membuat halaman
+#st.page ini butuh argumen; Halaman Utama/halaman_utama.py (path directory); 
 
 Mahasiswa1 = st.Page(
     "Buku Kating/016_Kemas Veriandra Ramadhan.py",
     title="016 - Kemas Veriandra Ramadhan",
     icon=":material/person:",
 )
+#Mahasiswa1 akan mengakses data yang bernama Kemas Veriandra yang foldernya disimpan di Buku Kating
+
 Mahasiswa2 = st.Page(
     "Buku Kating/020_Try Yani Rizki Nur Rohmah.py",
     title="020 - Try Yani Rizki Nur Rohmah",
@@ -38,6 +44,11 @@ Mahasiswa6 = st.Page(
     title="103 - Rut Junita Sari Siburian",
     icon=":material/person:",
 )
+Mahasiswa7 = st.Page(
+    "Buku Kating/050_Ahmad Rizky.py",
+    title="050-Ahmad Rizky",
+    icon=":material/person:",
+)
 
 
 #Perlu diperhatikan perubahannya
@@ -49,11 +60,14 @@ if st.session_state.pindah:
     pg = st.navigation(
         {
             "Halaman Utama": [Homepage],
-            "Buku Kating": [Mahasiswa1, Mahasiswa2, Mahasiswa3, Mahasiswa4, Mahasiswa5, Mahasiswa6],
+            "Buku Kating": [Mahasiswa1, Mahasiswa2, Mahasiswa3, Mahasiswa4, Mahasiswa5, Mahasiswa6, Mahasiswa7],
             "Try Me !!": [KREASI, KREASII],
         }
     )
 else:
     st.write("Maaf Anda kurang beruntung :(") 
 pg.run()
-
+#if state adalah logic di mana kita bisa berpindah-pindah page tanpa data-data kita ini hilang atau berubah
+#Halaman utama akan menampung homepage
+#Buku Kating akan menampung Nama mahasiswa
+#try me akan menampung kreasI dan kreasII
