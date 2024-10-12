c2c6581f20a9cab3e9743ab90f8884653404413f
fiodoraalysa20 <fiodoraalysajuandi@gmail.com>
2024-10-12 19:39:25 +0700

    buting

--- Buku Kating/081_Intan Nursyabani.py
+++ Buku Kating/081_Intan Nursyabani.py
@@ -104,7 +104,7 @@ menu = streamlit_menu()
 if menu == "kesekjenan":
 
     def kesekjenan():
-        gambar_url
+        gambar_urls = [
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
             "https://drive.google.com/file/d/1bR2tiDjlAqOvwHCTU3_GSrBUtDnZ9VHv/view",
         ]
